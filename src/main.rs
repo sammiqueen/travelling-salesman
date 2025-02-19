a86ea7e62eeb69d1f48a0d7e68507e1e9e77b0d6
Samantha Ågren <sammi.agren@elev.ga.ntig.se>
2025-02-19 08:57:33 +0100

    fn out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
     println!("Hello, world!");
+
+    let string = "Hello";
+    println!("{}", string);
+}
+
+fn cities_spawn() {
+    
+
+    return cities;
+}
+
+fn naive(cities : struct) {
+    
+    return solution;
+}
+
+fn nearest_neighbour(cities : struct) {
+
+    return solution;
+}
+
+fn christofide_serdyukov(cities : struct) {
+
+    return solution;
 }

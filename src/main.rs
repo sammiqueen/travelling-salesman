7d36adaf47dec1c1cba1dd57c6a6e30653ecd9df
Samantha Ågren <sammi.agren@elev.ga.ntig.se>
2025-02-21 11:09:27 +0100

    cities_spawn not working, fix by correctly implementing randomizing unique positions for the cities on a 10x10 grid (vec cities) and then calculating the distance from each city to each other city (make new fn calculate_distance)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use ndarray::Array;
 use ndarray::Ix2;
 use ndarray_rand::RandomExt;
@@ -15,29 +17,47 @@ fn main() {
 
 fn cities_spawn() -> Array::<i32, Ix2> {
     let cities_distances = Array::<i32, Ix2>::random((10, 10), Uniform::new(1, 10));
-    println!("{}", cities_distances);
+    
+    struct City {
+        position_x: i32,
+        position_y: i32,
+    }
+
+    let rng = rand::rng();
+
+    let cities : Vec<City> = Vec::new();
+
+    for i in 0..10 {
+        cities.push(City {
+            position_x: rng.random(),
+            position_y: rng.random(),
+        }
+    );
+        println!("{:?}", cities[i]);mu
+    }
+
+    //println!("{}", cities_distances);
 
     return cities_distances
 }
 
 
 fn naive(cities_distances : &Array::<i32, Ix2>) {
-    for city in cities_distances {
-        println!("{:?}", city);
-    }
+
+    println!("Hello from naive solution");
 }
 
 fn repetitive_nearest_neighbour(cities_distances : &Array::<i32, Ix2>) {
     let mut shortest_distance : i32 = 10;
 
     for city in cities_distances {
-        println!("{:?}", &city);
+        println!("{:?} c", city);
         
-        println!("{}", &shortest_distance);
+        println!("{:?} s", shortest_distance);
 
-        if &city < &shortest_distance {
-            println!("{} is shorter than {}", &city, &shortest_distance);
-            shortest_distance = city;
+        if *city < shortest_distance {
+            println!("{} is shorter than {}", city, shortest_distance);
+            shortest_distance = *city;
         }
     }
     

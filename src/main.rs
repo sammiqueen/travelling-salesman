3b6d20800a5095e96e472ec4cafa9671ffe0d01e
Samantha Ågren <sammi.agren@elev.ga.ntig.se>
2025-02-21 09:40:31 +0100

    broken repititive_shortest_path fn, fixing

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,13 @@ use ndarray_rand::RandomExt;
 use ndarray_rand::rand_distr::Uniform;
 
 fn main() {
-    let _cities_distances: Array::<i32, Ix2> = cities_spawn();
+    let cities_distances: Array::<i32, Ix2> = cities_spawn();
+
+    naive(&cities_distances);
+
+    repetitive_nearest_neighbour(&cities_distances);
+
+    //christofide_serdyukov(&cities_distances);
 }
 
 fn cities_spawn() -> Array::<i32, Ix2> {
@@ -14,16 +20,29 @@ fn cities_spawn() -> Array::<i32, Ix2> {
     return cities_distances
 }
 
-/*
-fn naive(cities_distances : Array(10, 10)) {
+
+fn naive(cities_distances : &Array::<i32, Ix2>) {
+    for city in cities_distances {
+        println!("{:?}", city);
+    }
+}
+
+fn repetitive_nearest_neighbour(cities_distances : &Array::<i32, Ix2>) {
+    let mut shortest_distance : i32 = 10;
+
+    for city in cities_distances {
+        println!("{:?}", &city);
+        
+        println!("{}", &shortest_distance);
+
+        if &city < &shortest_distance {
+            println!("{} is shorter than {}", &city, &shortest_distance);
+            shortest_distance = city;
+        }
+    }
     
 }
 
-fn nearest_neighbour(cities_distances : Array(10, 10)) {
+/*fn christofide_serdyukov(cities_distances : &Array::<i32, Ix2>) {
 
-}
-
-fn christofide_serdyukov(cities_distances : Array(10, 10)) {
-
-}
-*/
+}*/

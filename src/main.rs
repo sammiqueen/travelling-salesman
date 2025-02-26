edf2c5249889332776f9a06b3a149dfba9773711
Samantha Ågren <sammi.agren@elev.ga.ntig.se>
2025-02-26 10:09:55 +0100

    cities_spawn finally working, starting work on naïve solution

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@ use rand::Rng;
 
 use ndarray::Array;
 use ndarray::Ix2;
-use ndarray_rand::RandomExt;
-use ndarray_rand::rand_distr::Uniform;
+
+#[derive(Debug)]
+#[derive(Copy)]
+#[derive(Clone)]
+#[derive(PartialEq)]
+struct City {
+    position_x: i32,
+    position_y: i32,
+}
 
 fn main() {
-    let cities_distances: Array::<i32, Ix2> = cities_spawn();
+    let cities_distances: Array::<f64, Ix2> = cities_spawn();
 
     naive(&cities_distances);
 
@@ -15,16 +22,9 @@ fn main() {
     //christofide_serdyukov(&cities_distances);
 }
 
-fn cities_spawn() -> Array::<i32, Ix2> {
-    let cities_distances = Array::<i32, Ix2>::random((10, 10), Uniform::new(1, 10));
-    
-    #[derive(Debug)]
-    struct City {
-        position_x: i32,
-        position_y: i32,
-    }
+fn cities_spawn() -> Array::<f64, Ix2> {
 
-    let mut rng = rand::thread_rng();
+    let mut rng = rand::rng();
 
     let cities : [City; 10] = {
         let mut temp = [City {
@@ -34,47 +34,65 @@ fn cities_spawn() -> Array::<i32, Ix2> {
 
         for i in 0..10 {
 
-            let is_duplicate = {
-                for y in 0..10 {
-                    if temp[i] = temp[y] || temp[i] == (City {
+            let mut is_duplicate = false;
+
+            for y in 0..10 {
+                match temp[i] {
+                     val if val == temp[y] => is_duplicate = true,
+                    City {
                         position_x: 0,
-                        position_y:0
-                    }) {
-                        true
-                    }
-                    else {
-                        false
-                    }
+                        position_y: 0
+                    } => is_duplicate = true,
+                    _ => is_duplicate = false
                 }
             }
             
-            if !is_duplicate {
+            if is_duplicate {
                 temp[i] = City {
-                    position_x: rng.gen_range(1..100),
-                    position_y: rng.gen_range(1..100)
+                    position_x: rng.random_range(1..100),
+                    position_y: rng.random_range(1..100)
                 };
             }
-            else {
-                temp
-            }
         }
-
+        temp
     };
 
     println!("{:?}", cities);
 
-    //println!("{}", cities_distances);
+    let cities_distances : Array::<f64, Ix2> = {
+        let mut temp : Array::<f64, Ix2> = Array::<f64, Ix2>::zeros((10, 10));
 
-    return cities_distances
+        for i in 0..10 {
+            for y in 0..10 {
+                temp[[i, y]] = distance(cities[i], cities[y]);
+            }
+        }
+
+        temp
+    };
+     
+    cities_distances
+}
+
+fn distance(city_a : City, city_b : City) -> f64 {
+    let x_dif : f64 = (city_a.position_x - city_b.position_x).abs().into();
+    let y_dif : f64 = (city_a.position_y - city_b.position_y).abs().into();
+
+    let distance : f64 = (x_dif + y_dif).sqrt();
+
+    distance
 }
 
 
-fn naive(cities_distances : &Array::<i32, Ix2>) {
-
+fn naive(cities_distances : &Array::<f64, Ix2>) -> f64 {
     println!("Hello from naive solution");
+
+    let mut route_length : f64 = 0;
+
+    route_length
 }
 
-/*fn repetitive_nearest_neighbour(cities_distances : &Array::<i32, Ix2>) {
+/*fn repetitive_nearest_neighbour(cities_distances : &Array::<f64, Ix2>) {
     let mut shortest_distance : i32 = 10;
 
     for city in cities_distances {
@@ -90,6 +108,6 @@ fn naive(cities_distances : &Array::<i32, Ix2>) {
     
 }*/
 
-/*fn christofide_serdyukov(cities_distances : &Array::<i32, Ix2>) {
+/*fn christofide_serdyukov(cities_distances : &Array::<f64, Ix2>) {
 
 }*/
